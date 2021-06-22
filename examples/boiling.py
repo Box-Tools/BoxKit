6879d718ea3e1c7a155d8ad2c5c0befcdf1313da
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2021-06-22 13:46:12 -0400

    new updates

--- examples/boiling.py
+++ examples/boiling.py
@@ -4,7 +4,7 @@ basedir   = '/home/akash/Box/Jarvis-DataShare/Bubble-Box-Sample/boiling-earth/'
 prefix    = 'INS_Pool_Boiling_hdf5_'
 
 filetags  = [0,5,10,15,20,25,30,35,40,45,50,55]  
-filenames = ["".join([basedir,'INS_Pool_Boiling_hdf5_',str(filetag).zfill(4)]) for filetag in filetags]
+filenames = ["".join([basedir,prefix,str(filetag).zfill(4)]) for filetag in filetags]
 
 datasets  = [box.read_dataset(filename) for filename in filenames]
 
