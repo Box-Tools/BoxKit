13ce58a0d08ed0cc414701f626dc80b43c25f88d
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2021-06-22 12:37:43 -0400

    add keys to blocks

--- bubblebox/library/domain/_block.py
+++ bubblebox/library/domain/_block.py
@@ -42,6 +42,7 @@ class Block(object):
                                                                          self.ymax,
                                                                          self.zmin,
                                                                          self.zmax) +
+                " - keys   : {}\n".format(self.keys) +
                 " - tag    : {}\n".format(self.tag))
 
     def __getitem__(self,key):
@@ -120,6 +121,7 @@ class Block(object):
         Private method for initialization
         """
         self.data = data
+        self.keys = data.keys
 
     def _set_attributes(self,attributes):
         """
