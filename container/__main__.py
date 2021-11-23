4e5fe44b3dc5b6dd18c526124f555b3478749ad4
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2021-11-23 04:54:46 -0500

    update container/main

--- container/__main__.py
+++ container/__main__.py
@@ -9,7 +9,7 @@ def run_container(basedir):
 
     bubblebox.build()
     bubblebox.pour()
-    bubblebox.execute('"./setup develop && ./setup clean && python3 tests/container && \
+    bubblebox.execute('"./setup develop && ./setup clean && \
                          /home/user/.local/bin/jupyter notebook --port=8888 --no-browser --ip=0.0.0.0"')
     bubblebox.clean()
 
