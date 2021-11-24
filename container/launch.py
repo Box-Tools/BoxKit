53a2fd13d0693aaafc72a9b5381a79910ec4cdd0
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2021-11-23 22:02:18 -0500

    updates

---
+++ container/launch.py
@@ -0,0 +1,21 @@
+import pymaple
+import os
+
+if __name__ == "__main__":
+    """
+    Launch the local container with jupyter notebook
+    """
+
+    basedir = os.getenv('PWD')+'/..'
+
+    # Publish container for use with other applications
+    bubblebox = pymaple.Maple(image='akashdhruv/bubblebox:latest',container='bubblebox',
+                              source=basedir,target='/home/mount/bubblebox')
+
+    bubblebox.build()
+    bubblebox.pour()
+    bubblebox.execute('./setup develop && ./setup clean && \
+                       /home/user/.local/bin/jupyter notebook --port=8888 --no-browser --ip=0.0.0.0')
+    bubblebox.rinse()
+    bubblebox.clean()
+    bubblebox.remove()
