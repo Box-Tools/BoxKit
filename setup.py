1917298e8e5d9dbc33bcda6e8f501dc9722c1243
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2022-02-16 21:13:20 -0500

    update formatting for setup

--- setup.py
+++ setup.py
@@ -5,48 +5,76 @@ import re
 from setuptools import setup, find_packages
 
 # get long description from README
-with open('README.md', mode='r') as readme:
+with open("README.md", mode="r") as readme:
     long_description = readme.read()
 
-with open('cbox/__meta__.py', mode='r') as source:
+with open("cbox/__meta__.py", mode="r") as source:
     content = source.read().strip()
-    metadata = {key: re.search(key + r'\s*=\s*[\'"]([^\'"]*)[\'"]', content).group(1)
-                for key in ['__pkgname__', '__version__', '__authors__',
-                            '__license__', '__description__']}
+    metadata = {
+        key: re.search(key + r'\s*=\s*[\'"]([^\'"]*)[\'"]', content).group(1)
+        for key in [
+            "__pkgname__",
+            "__version__",
+            "__authors__",
+            "__license__",
+            "__description__",
+        ]
+    }
 
 setup(
-    name                 = metadata['__pkgname__'],
-    version              = metadata['__version__'],
-    author               = metadata['__authors__'],
-    description          = metadata['__description__'],
-    license              = metadata['__license__'],
-    packages             = find_packages(where='./'),
-    package_dir          = {'': './'},
-    package_data         = {'': ['*.so']},
-    include_package_data = True,
-    long_description     = long_description)
+    name=metadata["__pkgname__"],
+    version=metadata["__version__"],
+    author=metadata["__authors__"],
+    description=metadata["__description__"],
+    license=metadata["__license__"],
+    packages=find_packages(where="./"),
+    package_dir={"": "./"},
+    package_data={"": ["*.so"]},
+    include_package_data=True,
+    long_description=long_description,
+)
 
-with open('bubblebox/__meta__.py', mode='r') as source:
+with open("bubblebox/__meta__.py", mode="r") as source:
     content = source.read().strip()
-    metadata = {key: re.search(key + r'\s*=\s*[\'"]([^\'"]*)[\'"]', content).group(1)
-                for key in ['__pkgname__', '__version__', '__authors__',
-                            '__license__', '__description__']}
+    metadata = {
+        key: re.search(key + r'\s*=\s*[\'"]([^\'"]*)[\'"]', content).group(1)
+        for key in [
+            "__pkgname__",
+            "__version__",
+            "__authors__",
+            "__license__",
+            "__description__",
+        ]
+    }
 
 # core dependancies
-DEPENDENCIES = ['numpy==1.21', 'h5py',    'h5pickle', 'pymorton',    'scikit-image', 
-                'dask',  'pyarrow', 'joblib',   'distributed', 'tqdm', 'zarr']
+DEPENDENCIES = [
+    "numpy==1.21",
+    "h5py",
+    "h5pickle",
+    "pymorton",
+    "scikit-image",
+    "dask",
+    "pyarrow",
+    "joblib",
+    "distributed",
+    "tqdm",
+    "zarr",
+]
 
 setup(
-    name                 = metadata['__pkgname__'],
-    version              = metadata['__version__'],
-    author               = metadata['__authors__'],
-    description          = metadata['__description__'],
-    license              = metadata['__license__'],
-    packages             = find_packages(where='./'),
-    package_dir          = {'': './'},
-    include_package_data = True,
-    long_description     = long_description,
-    classifiers          = ['Programming Language :: Python :: 3.8',
-                            'License :: OSI Approved :: MIT License', ],
-    install_requires     = DEPENDENCIES)
-
+    name=metadata["__pkgname__"],
+    version=metadata["__version__"],
+    author=metadata["__authors__"],
+    description=metadata["__description__"],
+    license=metadata["__license__"],
+    packages=find_packages(where="./"),
+    package_dir={"": "./"},
+    include_package_data=True,
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=DEPENDENCIES,
+)
