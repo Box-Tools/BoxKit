09ea9a8be3e41265c547fc6585ac2413fb796667
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2022-10-28 10:03:36 -0500

    update license in meta

--- boxkit/__meta__.py
+++ boxkit/__meta__.py
@@ -3,6 +3,6 @@
 __pkgname__ = "BoxKit"
 __version__ = "1.0"
 __authors__ = "Akash Dhruv"
-__license__ = "MIT License"
+__license__ = "Apache License"
 __copyright__ = "Copyright (c) Akash Dhruv 2021. All Rights Reserved."
 __description__ = "BoxKit Utility"
