c1b5bee360d78de9bcb91bdd4851421ed27766cb
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2022-12-05 12:08:23 -0600

    update

--- setup.py
+++ setup.py
@@ -69,6 +69,6 @@ setup(
     install_requires=DEPENDENCIES,
     cmdclass={
         "develop": bin_cmd.DevelopCmd,
-        "build": bin_cmd.BuildCmd,
+        "build_py": bin_cmd.BuildCmd,
     },
 )
