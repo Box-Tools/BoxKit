4cade2325ace1a895c69de36b68d1e0a45ceee4d
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2022-12-05 12:57:13 -0600

    change custom build command

--- bin/cmd.py
+++ bin/cmd.py
@@ -2,7 +2,7 @@
 import os
 import sys
 import subprocess
-from setuptools.command.build_py import build_py
+from setuptools.command.build import build
 from setuptools.command.develop import develop
 
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
@@ -13,34 +13,11 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 from cbox import cbox_build, cbox_install
 from boost import boost_install
 
+# custom command
+class CustomCmd:
+    """Custom command."""
 
-def _set_options(
-    with_cbox=0,
-    with_pyarrow=0,
-    with_zarr=0,
-    with_dask=0,
-    with_server=0,
-    enable_testing=0,
-):
-    """
-    Create environment file
-    """
-    with open("boxkit/options.py", "w") as optfile:
-
-        optfile.write(f"cbox={with_cbox}\n")
-        optfile.write(f"pyarrow={with_pyarrow}\n")
-        optfile.write(f"zarr={with_zarr}\n")
-        optfile.write(f"dask={with_dask}\n")
-        optfile.write(f"server={with_server}\n")
-        optfile.write(f"testing={enable_testing}\n")
-
-
-# custom build command
-# replaces the default build command for setup.py
-class BuildCmd(build_py):
-    """Custom build_py command."""
-
-    user_options = build_py.user_options + [
+    user_options = [
         ("with-cbox", None, "With C++ backend"),
         ("with-pyarrow", None, "With pyarrow data backend"),
         ("with-zarr", None, "With zarr data backend"),
@@ -50,7 +27,6 @@ class BuildCmd(build_py):
     ]
 
     def initialize_options(self):
-        build_py.initialize_options(self)
         self.with_cbox = 0
         self.with_pyarrow = 0
         self.with_zarr = 0
@@ -59,25 +35,44 @@ class BuildCmd(build_py):
         self.enable_testing = 0
 
     def finalize_options(self):
-        build_py.finalize_options(self)
+        pass
 
     def run(self):
 
-        build_py.run(self)
+        with open("boxkit/options.py", "w") as optfile:
+
+            optfile.write(f"cbox={self.with_cbox}\n")
+            optfile.write(f"pyarrow={self.with_pyarrow}\n")
+            optfile.write(f"zarr={self.with_zarr}\n")
+            optfile.write(f"dask={self.with_dask}\n")
+            optfile.write(f"server={self.with_server}\n")
+            optfile.write(f"testing={self.enable_testing}\n")
+
+
+# replaces the default build command for setup.py
+class BuildCmd(build, CustomCmd):
+    """Custom build command."""
+
+    user_options = build.user_options + CustomCmd.user_options
+
+    def initialize_options(self):
+        build.initialize_options(self)
+        CustomCmd.initialize_options(self)
+
+    def finalize_options(self):
+        build.finalize_options(self)
+        CustomCmd.finalize_options(self)
+
+    def run(self):
+
+        build.run(self)
 
         if self.with_cbox:
             cbox_build()
             cbox_install()
             boost_install()
 
-        _set_options(
-            self.with_cbox,
-            self.with_pyarrow,
-            self.with_zarr,
-            self.with_dask,
-            self.with_server,
-            self.enable_testing,
-        )
+        CustomCmd.run(self)
 
         subprocess.run(
             "cp boxkit/options.py build/lib/boxkit/.",
@@ -87,31 +82,19 @@ class BuildCmd(build_py):
         )
 
 
-# custom develop command
 # replaces custom develop command for setup.py
-class DevelopCmd(develop):
+class DevelopCmd(develop, CustomCmd):
     """Custom develop command."""
 
-    user_options = develop.user_options + [
-        ("with-cbox", None, "With C++ backend"),
-        ("with-pyarrow", None, "With pyarrow data backend"),
-        ("with-zarr", None, "With zarr data backend"),
-        ("with-dask", None, "With dask data/parallel backend"),
-        ("with-server", None, "With remote server utilitiy"),
-        ("enable-testing", None, "Enable testing mode"),
-    ]
+    user_options = develop.user_options + CustomCmd.user_options
 
     def initialize_options(self):
         develop.initialize_options(self)
-        self.with_cbox = 0
-        self.with_pyarrow = 0
-        self.with_zarr = 0
-        self.with_dask = 0
-        self.with_server = 0
-        self.enable_testing = 0
+        CustomCmd.initialize_options(self)
 
     def finalize_options(self):
         develop.finalize_options(self)
+        CustomCmd.finalize_options(self)
 
     def run(self):
 
@@ -120,11 +103,4 @@ class DevelopCmd(develop):
         if self.with_cbox:
             cbox_build()
 
-        _set_options(
-            self.with_cbox,
-            self.with_pyarrow,
-            self.with_zarr,
-            self.with_dask,
-            self.with_server,
-            self.enable_testing,
-        )
+        CustomCmd.run(self)
