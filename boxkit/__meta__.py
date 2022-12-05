cd6ffb21c0a1c9765543db1230cbcbabd60e001b
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2022-12-05 16:21:42 -0600

    update version

--- boxkit/__meta__.py
+++ boxkit/__meta__.py
@@ -1,7 +1,7 @@
 """Metadata for BoxKit package"""
 
 __pkgname__ = "BoxKit"
-__version__ = "2.6"
+__version__ = "2.7"
 __authors__ = "Akash Dhruv"
 __license__ = "Apache License"
 __copyright__ = "Copyright (c) Akash Dhruv 2021. All Rights Reserved."
