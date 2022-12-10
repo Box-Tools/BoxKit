eb8f45cdf3f948bb9ce534b72b156d95970a1283
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2022-12-09 18:47:58 -0600

    remove dev notes

--- boxkit/api/reshape/_mergeblocks.py
+++ boxkit/api/reshape/_mergeblocks.py
@@ -73,15 +73,6 @@ def Mergeblocks(dataset, varlist, level=1, nthreads=1, monitor=False, backend="s
             origin=[merged_dataset.xmin, merged_dataset.ymin, merged_dataset.zmin]
         )
 
-        # TODO - this statement behaves differently for
-        # test datasets vs logical way - investigate. It maybe because
-        # test datasets were not constructured properly and maybe its time
-        # to build a new test datasets after all
-        #
-        # for test data
-        # blocklist_sorted[kloc + nblockx * jloc + nblockx * nblocky * iloc] = block
-        #
-        # desired
         blocklist_sorted[iloc + nblockx * jloc + nblockx * nblocky * kloc] = block
 
     for varkey in varlist:
@@ -124,20 +115,6 @@ def map_blk_to_merged_dset(unit, merged_dataset, varkey):
         origin=[merged_dataset.xmin, merged_dataset.ymin, merged_dataset.zmin]
     )
 
-    # TODO - this statement behaves differently for
-    # test datasets vs desired way - investigate. It maybe because
-    # test datasets were not constructured properly and maybe its time
-    # to build a new test datasets after all
-    #
-    # for test datasets
-    # merged_dataset[varkey][
-    #     0,
-    #     unit.nxb * iloc : unit.nxb * (iloc + 1),
-    #     unit.nyb * jloc : unit.nyb * (jloc + 1),
-    #     unit.nzb * kloc : unit.nzb * (kloc + 1),
-    # ] = unit[varkey][:, :, :]
-    #
-    # desired
     merged_dataset[varkey][
         0,
         unit.nzb * kloc : unit.nzb * (kloc + 1),
