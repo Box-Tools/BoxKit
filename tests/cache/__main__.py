1cb5e22471759ed5f9f6a371dea9c834b0d5e6f7
akashdhruv <akashdhruv@gwmail.gwu.edu>
2022-12-11 23:58:29 -0600

    seprate out tests for cache

--- tests/cache/__main__.py
+++ tests/cache/__main__.py
@@ -5,7 +5,7 @@ import unittest
 
 
 def main():
-    tests = ["boiling", "heater", "application"]
+    tests = ["mergeblocks"]
 
     suite = unittest.TestSuite()
 
--- tests/cache/mergeblocks.py
+++ tests/cache/mergeblocks.py
@@ -1,23 +1,19 @@
 """Tests for `boxkit/api`."""
 
 import os
-import time
 import unittest
 import pymorton
-import tqdm
 import numpy
 import h5py
-
-from progress.spinner import PieSpinner as Spinner
-
+import argparse
 import boxkit
 from boxkit.library import Timer, Action, Dataset
 
 
-class TestCache(unittest.TestCase):
+class TestMergeBlocks(unittest.TestCase):
     """boxkit unit test for 3D boiling data"""
 
-    def customSetUp(self, basedir):
+    def setUp(self):
         """
         Setup test parameters
 
@@ -32,11 +28,22 @@ class TestCache(unittest.TestCase):
             f"\n-------------------------Running: {self.id()}-------------------------\n"
         )
 
-        self.timer = Timer(self.id())
-        basedir = (
-            os.getenv("HOME")
-            + f"/Box/Jarvis-DataShare/Bubble-Box-Sample/boiling-earth/domain3D/{basedir}/"
-        )
+        test_parser = argparse.ArgumentParser(description="Test Parser")
+        test_parser.add_argument("-s", "--site", help="Test Site", type=str)
+        args = test_parser.parse_args()
+
+        if args.site == "sedona":
+            basedir = (
+                os.getenv("HOME")
+                + f"/Box/Jarvis-DataShare/Bubble-Box-Sample/boiling-earth/domain3D/not-chunked/"
+            )
+
+        elif args.site == "summit":
+            raise NotImplementedError
+
+        else:
+            raise NotImplementedError
+
         filetags = [*range(0, 58, 10)]
         prefix = "INS_Pool_Boiling_hdf5_"
         self.filenames = [
@@ -58,35 +65,47 @@ class TestCache(unittest.TestCase):
         """
         Test optimize implementation
         """
-        self.customSetUp("not-chunked")
+        timer_test = Timer(self.id())
 
         dataframes = [
-            boxkit.read_dataset(filename, storage="numpy")
+            boxkit.read_dataset(filename, storage="numpy-memmap")
             for filename in self.filenames
         ]
 
-        nthreads, subthreads = [1, 8]
+        mean = []
 
-        print(f"nthreads: {nthreads}, subthreads: {subthreads}")
+        nthreads = 1
+        print(f"nthreads: {nthreads}")
 
-        merge_dset_list.nthreads = nthreads
-        merge_dset_list.backend = "serial"
+        for dataset in dataframes:
+            timer_mergeblock_opt = Timer("[mergblocks.optimized]")
 
-        mean = merge_dset_list(dataframes, subthreads=subthreads)
+            merged_dataset = boxkit.mergeblocks(
+                dataset,
+                ["uvel", "vvel", "wvel", "temp", "phi"],
+                nthreads=nthreads,
+                backend="loky",
+            )
+
+            del timer_mergeblock_opt
+
+            mean.append(numpy.mean(merged_dataset["vvel"][:]))
+
+            merged_dataset.purge()
+            del merged_dataset
 
         for dataset in dataframes:
             dataset.purge()
 
         self.assertEqual(mean, self.mean_refs)
+        del timer_test
 
     def test_02_naive_3D(self):
         """
         Test naiver implementation
         """
-        self.customSetUp("not-chunked")
-
+        timer_test = Timer(self.id())
         dataframes = [h5py.File(filename, "r") for filename in self.filenames]
-
         mean = []
 
         for dataset in dataframes:
@@ -142,97 +161,11 @@ class TestCache(unittest.TestCase):
             del merged_uvel, merged_vvel, merged_wvel, merged_temp, merged_phi
 
         self.assertEqual(mean, self.mean_refs)
-
-    def test_03_naive_blk_dset_3D(self):
-        """
-        Test naiver implementation
-        """
-        self.customSetUp("block-dset")
-
-        dataframes = [h5py.File(filename, "r") for filename in self.filenames]
-
-        mean = []
-
-        for dataset in dataframes:
-
-            timer_mergeblocks_naive = Timer("[mergeblocks.naive]")
-
-            nxb, nyb, nzb = dataset["sizebox"][:]
-            nblockx, nblocky, nblockz = dataset["numbox"][:]
-
-            merged_uvel = numpy.zeros([nblockz * nzb, nblocky * nyb, nblockx * nxb])
-            merged_vvel = numpy.zeros_like(merged_uvel)
-            merged_wvel = numpy.zeros_like(merged_uvel)
-            merged_temp = numpy.zeros_like(merged_uvel)
-            merged_phi = numpy.zeros_like(merged_uvel)
-
-            for lblock in range(nblockx * nblocky * nblockz):
-                iloc, jloc, kloc = pymorton.deinterleave3(lblock)
-
-                merged_uvel[
-                    nzb * kloc : nzb * (kloc + 1),
-                    nyb * jloc : nyb * (jloc + 1),
-                    nxb * iloc : nxb * (iloc + 1),
-                ] = dataset["quantities"]["uvel"][str(lblock)][:, :, :]
-
-                merged_vvel[
-                    nzb * kloc : nzb * (kloc + 1),
-                    nyb * jloc : nyb * (jloc + 1),
-                    nxb * iloc : nxb * (iloc + 1),
-                ] = dataset["quantities"]["vvel"][str(lblock)][:, :, :]
-
-                merged_wvel[
-                    nzb * kloc : nzb * (kloc + 1),
-                    nyb * jloc : nyb * (jloc + 1),
-                    nxb * iloc : nxb * (iloc + 1),
-                ] = dataset["quantities"]["wvel"][str(lblock)][:, :, :]
-
-                merged_temp[
-                    nzb * kloc : nzb * (kloc + 1),
-                    nyb * jloc : nyb * (jloc + 1),
-                    nxb * iloc : nxb * (iloc + 1),
-                ] = dataset["quantities"]["temp"][str(lblock)][:, :, :]
-
-                merged_phi[
-                    nzb * kloc : nzb * (kloc + 1),
-                    nyb * jloc : nyb * (jloc + 1),
-                    nxb * iloc : nxb * (iloc + 1),
-                ] = dataset["quantities"]["phi"][str(lblock)][:, :, :]
-
-                self.spinner.next()
-            del timer_mergeblocks_naive
-
-            mean.append(numpy.mean(merged_vvel[:]))
-
-            del merged_uvel, merged_vvel, merged_wvel, merged_temp, merged_phi
-
-        self.assertEqual(mean, self.mean_refs)
+        del timer_test
 
     def tearDown(self):
         """Clean up and timing"""
         self.spinner.finish()
-        del self.timer
-
-
-@Action(unit=Dataset, nthreads=1, backend="loky")
-def merge_dset_list(unit, subthreads):
-    timer_mergeblock_opt = Timer("[mergblocks.optimized]")
-
-    merged_dataset = boxkit.mergeblocks(
-        unit,
-        ["uvel", "vvel", "wvel", "temp", "phi"],
-        nthreads=subthreads,
-        backend="loky",
-    )
-
-    del timer_mergeblock_opt
-
-    mean = numpy.mean(merged_dataset["vvel"][:])
-
-    merged_dataset.purge()
-    del merged_dataset
-
-    return mean
 
 
 if __name__ == "__main__":
