984f059e71581f28c636e485d8045d9525e269bf
akashdhruv <akashdhruv@gwmail.gwu.edu>
2023-06-21 15:21:10 -0500

    update some methods

--- boxkit/api/_regionprops.py
+++ boxkit/api/_regionprops.py
@@ -58,8 +58,12 @@ def skimage_props_blk(block, lsetkey, labelkey):
 
     listprops = skimage_measure.regionprops(block[labelkey].astype(int))
 
-    listprops = [
-        {"area": props["area"] * block.dx * block.dy * block.dz} for props in listprops
-    ]
+    proplist = ["area", "centroid", "equivalent_diameter_area"]
+
+    # listprops = [
+    #    {"area": props["area"] * block.dx * block.dy * block.dz} for props in listprops
+    # ]
+
+    listprops = [{key: props[key] for key in proplist} for props in listprops]
 
     return listprops
