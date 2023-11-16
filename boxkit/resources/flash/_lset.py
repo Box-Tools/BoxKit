6e95153b639f85f8728e9dd6e04bd0605090dff6
akashdhruv <akashdhruv@gwmail.gwu.edu>
2023-11-16 02:05:51 -0600

    add numba optimizations

--- boxkit/resources/flash/_lset.py
+++ boxkit/resources/flash/_lset.py
@@ -3,6 +3,7 @@
 import numpy
 import boxkit
 import skimage.measure as skimage_measure
+from numba import jit
 
 
 def lset_plot_contour_2d(ax, merged_dataset, filled=False, *args, **kwargs):
@@ -136,125 +137,209 @@ def lset_compute_normals_2d(dataset, varlist):
     dataset.fill_guard_cells(varlist)
 
 
-def lset_shape_measurement_2d(merged_dataset):
+def lset_vel_measurement_2d(merged_dataset):
     """
-    Perform measurements on the bubble
+    Perform velocity measurement on level set
+    """
+    if len(merged_dataset.blocklist) > 1:
+        raise ValueError(
+            "[boxkit.resources.flash.lset_vel_measurement_2d] dataset must only have one block"
+        )
+
+    merged_dataset.addvar("bwlabel", dtype=int)
+
+    for block in merged_dataset.blocklist:
+        block["bwlabel"] = skimage_measure.label(block["dfun"] >= 0)
+        mean_vel = numpy.zeros([numpy.max(block["bwlabel"][:]), 2], dtype=float)
+        mean_label = numpy.zeros([numpy.max(block["bwlabel"][:]), 1], dtype=float)
+
+        lset_vel_blk_2d(
+            block["dfun"],
+            block["bwlabel"],
+            block["velx"],
+            block["vely"],
+            block.nxb,
+            block.nyb,
+            block.xguard,
+            block.yguard,
+            block.dx,
+            block.dy,
+            mean_vel,
+            mean_label,
+        )
+
+    mean_vel = mean_vel / mean_label
+
+    merged_dataset.delvar("bwlabel")
+
+    return mean_vel
+
+
+def lset_shape_measurement_2d(merged_dataset, correction=False):
+    """
+    Perform shape measurement on level set
     """
     if len(merged_dataset.blocklist) > 1:
         raise ValueError(
             "[boxkit.resources.flash.lset_shape_measurement_2d] dataset must only have one block"
         )
 
-    bubblelist = boxkit.regionprops(merged_dataset, "dfun", backend="loky", nthreads=1)
+    bubblelist = boxkit.regionprops(merged_dataset, "dfun")
 
-    merged_dataset.addvar("bwlabel", dtype=int)
-    merged_dataset.addvar("nrmx", dtype=float)
-    merged_dataset.addvar("nrmy", dtype=float)
+    if correction:
 
-    for block in merged_dataset.blocklist:
-        block["bwlabel"] = skimage_measure.label(block["dfun"] >= 0)
+        merged_dataset.addvar("bwlabel", dtype=int)
+        merged_dataset.addvar("nrmx", dtype=float)
+        merged_dataset.addvar("nrmy", dtype=float)
 
-    lset_compute_normals_2d(merged_dataset, ["nrmx", "nrmy"])
+        lset_compute_normals_2d(merged_dataset, ["nrmx", "nrmy"])
 
-    modified_perimeter = [0.0] * len(bubblelist)
+        modified_perimeter = numpy.zeros([len(bubblelist), 1], dtype=float)
+        sol_points = numpy.zeros([2, 2], dtype=float)
 
-    for block in merged_dataset.blocklist:
+        for block in merged_dataset.blocklist:
+            block["bwlabel"] = skimage_measure.label(block["dfun"] >= 0)
+            xcenter = block.xrange("center")
+            ycenter = block.yrange("center")
 
-        xcenter = block.xrange("center")
-        ycenter = block.yrange("center")
+            lset_perimeter_blk_2d(
+                block["dfun"],
+                block["bwlabel"],
+                block["nrmx"],
+                block["nrmy"],
+                xcenter,
+                ycenter,
+                modified_perimeter,
+                block.nxb,
+                block.nyb,
+                block.xguard,
+                block.yguard,
+                block.dx,
+                block.dy,
+                sol_points,
+            )
 
-        for k in range(block.zguard, block.nzb + block.zguard):
-            for j in range(block.yguard, block.nyb + block.yguard):
-                for i in range(block.xguard, block.nxb + block.xguard):
-                    if (
-                        False
-                        or (block["dfun"][k, j, i] * block["dfun"][k, j, i - 1] <= 0)
-                        or (block["dfun"][k, j, i] * block["dfun"][k, j, i + 1] <= 0)
-                        or (block["dfun"][k, j, i] * block["dfun"][k, j - 1, i] <= 0)
-                        or (block["dfun"][k, j, i] * block["dfun"][k, j + 1, i] <= 0)
-                    ):
-                        labels = [
-                            block["bwlabel"][k, j, i],
-                            block["bwlabel"][k, j, i + 1],
-                            block["bwlabel"][k, j, i - 1],
-                            block["bwlabel"][k, j + 1, i],
-                            block["bwlabel"][k, j - 1, i],
-                        ]
+        for bubble_index, bubble in enumerate(bubblelist):
+            bubble["perimeter"] = modified_perimeter[bubble_index]
 
-                        labels = list(set(labels))
-
-                        if 0 not in labels:
-                            raise ValueError(
-                                f"[boxkit.resources.flash.bubble_shape_measurement] 0 does not exist in "
-                                + f"{labels} near point {[k,j,i]}"
-                            )
-
-                        labels.pop(0)
-                        if len(labels) > 1:
-                            raise ValueError(
-                                f"[boxkit.resources.flash.bubble_shape_measurement] More than 1 "
-                                + f"non-zeros values exist in {labels} near point {[k,j,i]}"
-                            )
-
-                        bubble_index = labels[0] - 1
-
-                        if abs(block["dfun"][k, j, i]) <= numpy.sqrt(
-                            block.dx**2 + block.dy**2
-                        ):
-
-                            xquery, yquery, = [
-                                xcenter[i]
-                                + block["dfun"][k, j, i] * block["nrmx"][k, j, i],
-                                ycenter[j]
-                                + block["dfun"][k, j, i] * block["nrmy"][k, j, i],
-                            ]
-
-                            line_slope = (
-                                -block["nrmx"][k, j, i] / block["nrmy"][k, j, i]
-                            )
-                            line_constant = yquery - line_slope * xquery
-
-                            xlow, xhigh = [
-                                xcenter[i] - block.dx / 2,
-                                xcenter[i] + block.dx / 2,
-                            ]
-                            ylow, yhigh = [
-                                ycenter[j] - block.dy / 2,
-                                ycenter[j] + block.dy / 2,
-                            ]
-
-                            p1 = [xlow, line_slope * xlow + line_constant]
-                            p2 = [xhigh, line_slope * xhigh + line_constant]
-                            p3 = [(ylow - line_constant) / line_slope, ylow]
-                            p4 = [(yhigh - line_constant) / line_slope, yhigh]
-
-                            num_points = 0
-                            sol_points = numpy.zeros([2, 2], dtype=float)
-
-                            for point in [p1, p2, p3, p4]:
-                                if (
-                                    True
-                                    and point[0] >= xlow
-                                    and point[0] <= xhigh
-                                    and point[1] >= ylow
-                                    and point[1] <= yhigh
-                                ):
-                                    sol_points[num_points, 0] = point[0]
-                                    sol_points[num_points, 1] = point[1]
-                                    num_points = num_points + 1
-
-                            modified_perimeter[bubble_index] = modified_perimeter[
-                                bubble_index
-                            ] + numpy.sqrt(
-                                (sol_points[1, 1] - sol_points[0, 1]) ** 2
-                                + (sol_points[1, 0] - sol_points[0, 0]) ** 2
-                            )
-
-    for bubble_index, bubble in enumerate(bubblelist):
-        bubble["perimeter"] = modified_perimeter[bubble_index]
-
-    merged_dataset.delvar("bwlabel")
-    merged_dataset.delvar("nrmx")
-    merged_dataset.delvar("nrmy")
+        merged_dataset.delvar("bwlabel")
+        merged_dataset.delvar("nrmx")
+        merged_dataset.delvar("nrmy")
 
     return bubblelist
+
+
+@jit(nopython=True)
+def lset_vel_blk_2d(
+    dfun, label, velx, vely, nxb, nyb, xguard, yguard, dx, dy, mean_vel, mean_label
+):
+    k = 0
+    for j in range(yguard, nyb + yguard):
+        for i in range(xguard, nxb + xguard):
+            if label[k, j, i] > 0:
+                bubble_index = label[k, j, i] - 1
+                mean_vel[bubble_index, 0] = (
+                    mean_vel[bubble_index, 0] + vely[k, j, i] * dx * dy
+                )
+                mean_vel[bubble_index, 1] = (
+                    mean_vel[bubble_index, 1] + velx[k, j, i] * dx * dy
+                )
+                mean_label[bubble_index] = mean_label[bubble_index] + dx * dy
+
+
+@jit(nopython=True)
+def lset_perimeter_blk_2d(
+    dfun,
+    label,
+    nrmx,
+    nrmy,
+    xcenter,
+    ycenter,
+    perimeter,
+    nxb,
+    nyb,
+    xguard,
+    yguard,
+    dx,
+    dy,
+    sol_points,
+):
+    """
+    Get perimeter using level-set
+    """
+    k = 0
+    for j in range(yguard, nyb + yguard):
+        for i in range(xguard, nxb + xguard):
+            if (
+                (dfun[k, j, i] * dfun[k, j, i - 1] <= 0)
+                or (dfun[k, j, i] * dfun[k, j, i + 1] <= 0)
+                or (dfun[k, j, i] * dfun[k, j - 1, i] <= 0)
+                or (dfun[k, j, i] * dfun[k, j + 1, i] <= 0)
+            ):
+                labels = [
+                    label[k, j, i],
+                    label[k, j, i + 1],
+                    label[k, j, i - 1],
+                    label[k, j + 1, i],
+                    label[k, j - 1, i],
+                ]
+
+                bubble_index = (
+                    list(set([label for label in labels if label != 0]))[0] - 1
+                )
+
+                if abs(dfun[k, j, i]) <= numpy.sqrt(dx**2 + dy**2):
+
+                    lset_intersect_blk_2d(
+                        dfun[k, j, i],
+                        nrmx[k, j, i],
+                        nrmy[k, j, i],
+                        xcenter[i],
+                        ycenter[j],
+                        dx,
+                        dy,
+                        sol_points,
+                    )
+
+                    perimeter[bubble_index] = perimeter[bubble_index] + numpy.sqrt(
+                        (sol_points[1, 1] - sol_points[0, 1]) ** 2
+                        + (sol_points[1, 0] - sol_points[0, 0]) ** 2
+                    )
+
+
+@jit(nopython=True)
+def lset_intersect_blk_2d(dfun, nrmx, nrmy, xcenter, ycenter, dx, dy, sol_points):
+    """
+    Get intersection of level-set with block
+    """
+    (
+        xquery,
+        yquery,
+    ) = [xcenter + dfun * nrmx, ycenter + dfun * nrmy]
+
+    line_slope = -nrmx / nrmy
+    line_constant = yquery - line_slope * xquery
+
+    xlow, xhigh = [xcenter - dx / 2, xcenter + dx / 2]
+    ylow, yhigh = [ycenter - dy / 2, ycenter + dy / 2]
+
+    p1 = [xlow, line_slope * xlow + line_constant]
+    p2 = [xhigh, line_slope * xhigh + line_constant]
+    p3 = [(ylow - line_constant) / line_slope, ylow]
+    p4 = [(yhigh - line_constant) / line_slope, yhigh]
+
+    num_points = 0
+    sol_points[:] = 0.0
+
+    for point in [p1, p2, p3, p4]:
+        if (
+            point[0] >= xlow
+            and point[0] <= xhigh
+            and point[1] >= ylow
+            and point[1] <= yhigh
+        ):
+            sol_points[num_points, 0] = point[0]
+            sol_points[num_points, 1] = point[1]
+            num_points = num_points + 1
+
+    return sol_points
